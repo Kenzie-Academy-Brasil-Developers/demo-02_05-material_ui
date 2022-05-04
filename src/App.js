ee129908bd1ade73d419612741827f9bb57cdcf4
Paulo Mello <paulo@kenzie.com.br>
2022-05-04 18:42:01 -0300

    feat: dark mode with material UI, build: change React and React-dom version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import {
+  Link,
+  Button,
+  Grid,
+  Box,
+  Avatar,
+  Container,
+  TextField,
+  CssBaseline,
+  createTheme,
+  Switch,
+  FormControlLabel,
+} from "@mui/material";
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
+import { ThemeProvider } from "@emotion/react";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("dark");
+
+  const darkTheme = () =>
+    createTheme({
+      palette: {
+        mode,
+      },
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={darkTheme}>
+      <FormControlLabel
+        control={
+          <Switch
+            checked={mode === "dark" ? true : false}
+            onChange={() =>
+              setMode((oldValue) => (oldValue === "light" ? "dark" : "light"))
+            }
+            name="Light"
+          />
+        }
+        label={mode === "light" ? "Dark" : "Light"}
+      />
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            mt: 10,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Avatar sx={{ m: 2, bgcolor: "secondary.main" }}>
+            <VpnKeyIcon />
+          </Avatar>
+
+          {/* mudar para Typography  */}
+          <h1>Login</h1>
+
+          {/* Fazer o form usando o Box */}
+
+          <TextField
+            label="Email"
+            type="email"
+            fullWidth
+            margin="normal"
+            error="true"
+            helperText="Email inválido"
+          />
+          <TextField label="Senha" type="password" fullWidth margin="normal" />
+
+          {/* Construir um FormControlLabel com um Remember me */}
+
+          <Button fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Enviar
+          </Button>
+
+          <Grid container xs={12}>
+            <Grid item xs>
+              <Link>Esqueceu a senha?</Link>
+            </Grid>
+            <Grid item>
+              <Link>Ainda não tem cadastro?</Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
